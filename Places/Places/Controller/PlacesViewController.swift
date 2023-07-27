bba689343b921bff4b0876a749083406a3ed9f44
mohamdan <engineerm86@gmail.com>
2023-07-27 15:25:28 +0300

    add PlaceCell

--- Places/Places/Controller/PlacesViewController.swift
+++ Places/Places/Controller/PlacesViewController.swift
@@ -12,19 +12,27 @@ class PlacesViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "PlaceCell", bundle: nil), forCellReuseIdentifier: "PlaceCell")
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+extension PlacesViewController : UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlaceCell", for: indexPath) as! PlaceCell
+        cell.name.text = "egypt"
+        cell.desc.text = "home"
+        cell.time.text = "12:00"
+        
+        return cell
+    }
+    
+    
 }
